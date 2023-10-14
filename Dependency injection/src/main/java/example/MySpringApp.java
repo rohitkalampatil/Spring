f4afeaee714edb11c265f7cae3c4eb2927928dc4
rohitkalampatil <rohitkalam96k@gmail.com>
2023-10-14 16:58:53 +0530

    Dependency injection -> scope of bean

--- Dependency injection/src/main/java/example/MySpringApp.java
+++ Dependency injection/src/main/java/example/MySpringApp.java
@@ -6,11 +6,18 @@ public class MySpringApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-		Vehicle vehicle = context.getBean("myBus",Vehicle.class);
-		System.out.println(vehicle.getMiledge());
-		System.out.println(vehicle.getDiscount());
-		System.out.println(vehicle.getBrandName());
-		System.out.println(vehicle.getPrice());
+		Vehicle vehicle1 = context.getBean("myBus", Vehicle.class);
+		Vehicle vehicle2 = context.getBean("myBus", Vehicle.class);
+
+		System.out.println(vehicle1 == vehicle2);	
+		
+		// true scope default = singleton
+		// false after declr scope="prototype" in config.xml
+		
+		System.out.println(vehicle1.getMiledge());
+		System.out.println(vehicle1.getDiscount());
+		System.out.println(vehicle1.getBrandName());
+		System.out.println(vehicle1.getPrice());
 	}
 
 }
